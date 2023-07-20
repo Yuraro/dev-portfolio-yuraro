5ae31c0f794dc2260bc24d6e2a0682dec8288b98
Yuraro <romanykyriy@gmail.com>
2023-07-19 19:30:42 -0700

    Update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <>
         <div className={css.homeWrap}>
             <div className={css.title_wrap}>
-                <h1 className={css.homeTitle}>Discover Your Ideal Choice: Learn More Within 2 Minutes, and It Might Just Be the Sign You've Been Waiting For!</h1>
+                <h1 className={css.homeTitle}>Discover Your Ideal Choice: Learn More Within <span className={css.timeHighlight}>2 Minutes</span>, and It Might Just Be the Sign You've Been Waiting For!</h1>
                 <button className={css.homeButton}>Start!</button>
             </div>
         </div>
