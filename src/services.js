91a85437866678207f85b4c1c49523a0e2b6f399
Yuraro <romanykyriy@gmail.com>
2023-07-23 12:58:21 -0700

    Update projects

--- src/services.js
+++ src/services.js
@@ -1,26 +1,26 @@
-import IceCreame from '../src/images/IceCreame.jpeg';
+import IceCream from '../src/images/IceCream.jpeg';
 import Cinemania from '../src/images/Cinemania.jpeg';
 
 
 const projectsData = [
     {
-    title: 'Ice Cream',
-    year: '2022',
-    githubLink: 'https://github.com/Yuraro/dev-portfolio-yuraro',
-    websiteLink: 'https://margomarm.github.io/team-project-icecream/',
-    description: 'Collaboratively designed and developed a visually appealing café website...',
-    technologies: ['JS', 'SASS', 'HTML', 'Parcel'],
-    image: IceCreame,
+    title: 'Cinemania',
+    year: '2023',
+    githubLink: 'https://github.com/nzend/team-project-cinemania',
+    websiteLink: 'https://nzend.github.io/team-project-cinemania/',
+    description: 'Contributed to the development of a collaborative movie search project,incorporating keyword search, dynamic content rendering, and personalized watchlists, highlighting strong teamwork and front-end development skills to deliver a user-friendly interface.',
+    technologies: ['JS', 'SASS', 'HTML', 'Parcel', 'Axios'],
+    image: Cinemania,
     },
     {
     title: 'Ice Cream',
     year: '2022',
-    githubLink: 'https://github.com/Yuraro/dev-portfolio-yuraro',
+    githubLink: 'https://github.com/MargoMarm/team-project-icecream',
     websiteLink: 'https://margomarm.github.io/team-project-icecream/',
-    description: 'Collaboratively designed and developed a visually appealing café website...',
+    description: 'Collaboratively designed and developed a visually appealing café website, showcasing an array of product offerings, capturing the cafés ambiance, and featuring location information. Implemented interactive elements, seamless navigation, and a unique brand identity, resulting in an enhanced user experience.',
     technologies: ['JS', 'SASS', 'HTML', 'Parcel'],
-    image: Cinemania,
-    }
+    image: IceCream,
+}
 ];
 
 export default projectsData;
