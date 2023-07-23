6fa8a9fa6ea92028d3572641b2f48141b2d0a8a9
Yuraro <romanykyriy@gmail.com>
2023-07-23 12:21:13 -0700

    Update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,42 +1,39 @@
 import React from 'react';
-import IceCreame from '../../images/IceCreame.jpeg'; 
 import css from './Projects.module.css';
+import projectsData from '../../services';
 
 const Projects = () => {
     return (
-        <div className={css.projects_wrap}>
-            <a href="https://margomarm.github.io/team-project-icecream/" target="_blank" rel="noopener noreferrer">
-                <img src={IceCreame} alt="Project Ice Cream" className={css.img_web} />
+    <div>
+        {projectsData.map((project, index) => (
+        <div className={css.projects_wrap} key={index}>
+            <a href={project.websiteLink} target="_blank" rel="noopener noreferrer">
+            <img src={project.image} alt={project.title} className={css.img_web} />
             </a>
             <div className={css.info_wrap}>
-                <h2 className={css.info_title}>
-                Ice Cream <span className={css.info_year}>(2022)</span>
-                </h2>
-                <a className={css.info_github} href="https://github.com/Yuraro/dev-portfolio-yuraro">GitHub</a>
-                <h3 className={css.info_description}>
-                    Collaboratively designed and developed a visually appealing café website, showcasing an array of product offerings, capturing the café's ambiance, and featuring location information. Implemented interactive elements, seamless navigation, and a unique brand identity, resulting in an enhanced user experience. <br/> Role: Developer
-                </h3>
-                <h3 className={css.info_tech_title}>
-                    Technologis used:
-                </h3>
-                <ul className={css.info_tech_list}>
-                    <li className={css.info_tech}>
-                        <p className={css.info_tech_item}>JS</p>
-                    </li>
-                    <li className={css.info_tech}>
-                        <p className={css.info_tech_item}>SASS</p>
-                    </li>
-                    <li className={css.info_tech}>
-                        <p className={css.info_tech_item}>HTML</p>
-                    </li>
-                    <li className={css.info_tech}>
-                        <p className={css.info_tech_item}>Parcel</p>
-                    </li>
-                </ul>
+            <h2 className={css.info_title}>
+                {project.title} <span className={css.info_year}>({project.year})</span>
+            </h2>
+            <a className={css.info_github} href={project.githubLink}>GitHub</a>
+            <h3 className={css.info_description}>
+                {project.description}
+            </h3>
+            <h3 className={css.info_tech_title}>
+                Technologies used:
+            </h3>
+            <ul className={css.info_tech_list}>
+                {project.technologies.map((tech, index) => (
+                <li className={css.info_tech} key={index}>
+                    <p className={css.info_tech_item}>{tech}</p>
+                </li>
+                ))}
+            </ul>
             </div>
         </div>
-    )
-
+        ))}
+    </div>
+    );
 };
 
 export default Projects;
+
---
+++ src/services.js
@@ -0,0 +1,26 @@
+import IceCreame from '../src/images/IceCreame.jpeg';
+import Cinemania from '../src/images/Cinemania.jpeg';
+
+
+const projectsData = [
+    {
+    title: 'Ice Cream',
+    year: '2022',
+    githubLink: 'https://github.com/Yuraro/dev-portfolio-yuraro',
+    websiteLink: 'https://margomarm.github.io/team-project-icecream/',
+    description: 'Collaboratively designed and developed a visually appealing café website...',
+    technologies: ['JS', 'SASS', 'HTML', 'Parcel'],
+    image: IceCreame,
+    },
+    {
+    title: 'Ice Cream',
+    year: '2022',
+    githubLink: 'https://github.com/Yuraro/dev-portfolio-yuraro',
+    websiteLink: 'https://margomarm.github.io/team-project-icecream/',
+    description: 'Collaboratively designed and developed a visually appealing café website...',
+    technologies: ['JS', 'SASS', 'HTML', 'Parcel'],
+    image: Cinemania,
+    }
+];
+
+export default projectsData;
